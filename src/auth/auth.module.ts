7698537f7b1875a8d358422c7bde60719a881676
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-06-10 10:39:53 +0700

    auth: add auth logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './user.entity';
+import { AuthService } from './auth.service';
 
-@Module({})
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  providers: [AuthService],
+})
 export class AuthModule {}
